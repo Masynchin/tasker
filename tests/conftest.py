c9fc46328dd26b5e2f539ec44eb93ed77edca418
Masynchin <masynchin@gmail.com>
2021-08-20 09:53:39 +0300

    add `unique_email` fixture to use `create_user` multiple times

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,12 +12,24 @@ async def open_test_db():
 
 
 @pytest.fixture
-def create_user():
+def unique_email():
+    prefix = "a"
+
+    def _unique_email():
+        nonlocal prefix
+        prefix = chr(ord(prefix) + 1)
+        return f"{prefix}@mail.com"
+
+    return _unique_email
+
+
+@pytest.fixture
+def create_user(unique_email):
     async def _create_user(
         email=None, username=None, password=None, role=None
     ):
         user_data = {
-            "email": email or "mail@mail.com",
+            "email": email or unique_email(),
             "username": username or "username",
             "password": password or "12345678",
             "role": role or "student",
